cc1d7b410b3276fbd6b77790d8d92cc35f83cb94
Lucas Miranda <lucasmirandasilva199@gmail.com>
2026-05-29 11:57:02 -0300

    atualizaçao bluepritns

--- ape/routes/auth_routes.py
+++ ape/routes/auth_routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from flask_login import login_user, logout_user, login_required, current_user
-from extensions import limiter
 from modules.auth import validar_login, User
 from utils.logger import log_erro
 from services.log_service import registrar_log
 from modules.permissoes import acesso_requerido
+from ape.extensions import limiter
 
 auth_bp = Blueprint("auth", __name__)
 

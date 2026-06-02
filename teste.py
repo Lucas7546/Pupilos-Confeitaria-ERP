ecea3cf555435b85af339c85e39878893b902fe7
Lucas Miranda <lucasmirandasilva199@gmail.com>
2026-06-02 16:55:24 -0300

    Adiciona assets locais

--- teste.py
+++ teste.py
@@ -4,16 +4,17 @@ from modules.db import get_conn
 load_dotenv()
 
 
+
+
 with get_conn() as conn:
     with conn.cursor() as cur:
 
         cur.execute("""
-        SELECT
-            column_name
-        FROM information_schema.columns
-        WHERE table_name = 'movimentacao_produtos'
-        ORDER BY ordinal_position
+            SELECT
+                id_produto,
+                nome
+            FROM produtos
         """)
 
-        for c in cur.fetchall():
-            print(c[0])
+        for linha in cur.fetchall():
+            print(linha)

661f407aaaae82f4618e7cc2251dac096c2ecb63
Lucas Miranda <lucasmirandasilva199@gmail.com>
2026-06-16 22:00:45 -0300

    arrumando enviar feedback

--- ape/routes/feedback_routes.py
+++ ape/routes/feedback_routes.py
@@ -9,29 +9,52 @@ feedback_bp = Blueprint("feedback", __name__)
 @feedback_bp.route("/enviar-feedback", methods=["POST"])
 @login_required
 def enviar_feedback():
+
     tipo = request.form.get("tipo")
     mensagem = request.form.get("mensagem", "").strip()
-    
+
     if not mensagem:
         flash("A mensagem não pode estar vazia.", "warning")
         return redirect(url_for("main.dashboard"))
-    
+
     try:
+
         with db_conn() as conn:
             with conn.cursor() as cur:
-                cur.execute("""
-                    INSERT INTO feedback (id_empresa, usuario_origem, tipo, mensagem)
-                    VALUES (%s, %s, %s, %s)
-                """, (g.id_empresa, current_user.username, tipo, mensagem))
-                conn.commit() # Garante que a inserção seja salva
-                
-        flash("Obrigado pelo seu feedback! Vamos analisar.", "success")
-    except Exception as e:
-        log_erro(f"Erro ao salvar feedback: {e}")
-        flash("Erro ao enviar feedback, tente novamente mais tarde.", "danger")
-        
-    redirect(url_for("main.dashboard"))
 
+                cur.execute("""
+                    INSERT INTO feedback
+                    (
+                        id_empresa,
+                        usuario_origem,
+                        tipo,
+                        mensagem
+                    )
+                    VALUES (%s, %s, %s, %s)
+                """, (
+                    g.id_empresa,
+                    current_user.username,
+                    tipo,
+                    mensagem
+                ))
+
+        flash(
+            "Obrigado pelo seu feedback! Vamos analisar.",
+            "success"
+        )
+
+    except Exception as e:
+
+        log_erro(
+            f"Erro ao salvar feedback: {e}"
+        )
+
+        flash(
+            "Erro ao enviar feedback, tente novamente mais tarde.",
+            "danger"
+        )
+
+    return redirect(url_for("main.dashboard"))
 
 @feedback_bp.route("/painel-feedback")
 @login_required # Ou coloque um check de admin aqui
